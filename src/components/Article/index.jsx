5dd8ee4e1db1c147f923e0eb55d24d23f6ded9cd
Thomas Quinton <cartmam1@hotmail.fr>
2024-01-22 13:06:51 +0100

    feat: Presentation page

---
+++ src/components/Article/index.jsx
@@ -0,0 +1,13 @@
+import styles from "/src/components/Article/index.module.scss";
+
+/**
+ * @param {Object} props
+ * @param {React.ReactNode} props.children
+ */
+export default function Article({children}) {
+    return (
+        <article className={styles.article}>
+            {children}
+        </article>
+    );
+}
